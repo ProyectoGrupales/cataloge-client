3f85029075048dd9404d983d5fa3700fb7d7aec1
EduBanegas <edu.leb14@gmail.com>
2022-06-20 16:14:04 -0300

    Added a carousel for brands in the products listed view

--- src/pages/[name]/[view]/index.js
+++ src/pages/[name]/[view]/index.js
@@ -1,6 +1,37 @@
+import { useRouter } from 'next/router';
+import HeadDefault from '../../../components/MetaHead/HeadDefault';
+import Header from '../../../components/Header/Header';
+import Carousel from '../../../components/Carousel/Carousel';
+
 // Esto es lo que ellos ven al clickear en una card
 const View = () => {
-	return <h1>Aqui estaría el listado o la info del producto</h1>;
+	const routes = useRouter();
+
+	if (routes.query.view && isNaN(routes.query.view)) {
+		return (
+			<>
+				<HeadDefault
+					title={'Listado De Productos'}
+					description={
+						'En esta sección podrá ver todos los productos relacionados a la categoría en forma de listado'
+					}
+				/>
+				<Header />
+
+				<Carousel />
+			</>
+		);
+	}
+
+	return (
+		<>
+			<HeadDefault
+				title={'Detalle Del Producto'}
+				description={'Información general del producto seleccionado'}
+			/>
+			<h1>Product Preview</h1>;
+		</>
+	);
 };
 
 export default View;
---
+++ src/pages/[name]/[view]/strings.js
@@ -0,0 +1,77 @@
+export default [
+	{
+		ProductName: 'Nombre del producto',
+		ProductCategory: 'Categoria del producto',
+		ProductPrice: '00,00',
+	},
+	{
+		ProductName: 'Nombre del producto',
+		ProductCategory: 'Categoria del producto',
+		ProductPrice: '00,00',
+	},
+	{
+		ProductName: 'Nombre del producto',
+		ProductCategory: 'Categoria del producto',
+		ProductPrice: '00,00',
+	},
+	{
+		ProductName: 'Nombre del producto',
+		ProductCategory: 'Categoria del producto',
+		ProductPrice: '00,00',
+	},
+	{
+		ProductName: 'Nombre del producto',
+		ProductCategory: 'Categoria del producto',
+		ProductPrice: '00,00',
+	},
+	{
+		ProductName: 'Nombre del producto',
+		ProductCategory: 'Categoria del producto',
+		ProductPrice: '00,00',
+	},
+	{
+		ProductName: 'Nombre del producto',
+		ProductCategory: 'Categoria del producto',
+		ProductPrice: '00,00',
+	},
+	{
+		ProductName: 'Nombre del producto',
+		ProductCategory: 'Categoria del producto',
+		ProductPrice: '00,00',
+	},
+	{
+		ProductName: 'Nombre del producto',
+		ProductCategory: 'Categoria del producto',
+		ProductPrice: '00,00',
+	},
+	{
+		ProductName: 'Nombre del producto',
+		ProductCategory: 'Categoria del producto',
+		ProductPrice: '00,00',
+	},
+	{
+		ProductName: 'Nombre del producto',
+		ProductCategory: 'Categoria del producto',
+		ProductPrice: '00,00',
+	},
+	{
+		ProductName: 'Nombre del producto',
+		ProductCategory: 'Categoria del producto',
+		ProductPrice: '00,00',
+	},
+	{
+		ProductName: 'Nombre del producto',
+		ProductCategory: 'Categoria del producto',
+		ProductPrice: '00,00',
+	},
+	{
+		ProductName: 'Nombre del producto',
+		ProductCategory: 'Categoria del producto',
+		ProductPrice: '00,00',
+	},
+	{
+		ProductName: 'Nombre del producto',
+		ProductCategory: 'Categoria del producto',
+		ProductPrice: '00,00',
+	},
+];
