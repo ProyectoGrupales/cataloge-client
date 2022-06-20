921b1a7a908048f61f7c6fe528e8538d13715531
BrunoAmerio <88524640+BrunoAmerio@users.noreply.github.com>
2022-06-20 15:46:16 -0300

    feature: cardCreator page

--- src/pages/admin/[name]/index.js
+++ src/pages/admin/[name]/index.js
@@ -1,5 +1,10 @@
 import Header from '../../../components/Header/Header';
 import Card from '../../../components/Card/Card';
+import Link from 'next/link';
+
+// Icons
+import AddIcon from '@mui/icons-material/Add';
+import style from '../../../styles/AdminCataloge.module.scss';
 
 // Data
 import catalogeData from '../../../data/cataloge.json';
@@ -14,6 +19,12 @@ const View = () => {
 					return <Card data={card} key={card.id} />;
 				})}
 			</div>
+
+			<Link href={'/admin/[name]/cardCreator'} as={'/admin/test/cardCreator'}>
+				<button className={style.addButton}>
+					<AddIcon fontSize='large' />
+				</button>
+			</Link>
 		</div>
 	);
 };
