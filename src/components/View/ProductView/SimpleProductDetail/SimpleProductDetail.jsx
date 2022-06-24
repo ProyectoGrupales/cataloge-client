c283e77eafc1d5b53e495265af83c986b1b3c26f
EduBanegas <edu.leb14@gmail.com>
2022-06-24 12:22:15 -0300

    #Comments: - Products are added to the catalog json. - Installation of spinners-react.

--- src/components/View/ProductView/SimpleProductDetail/SimpleProductDetail.jsx
+++ src/components/View/ProductView/SimpleProductDetail/SimpleProductDetail.jsx
@@ -6,40 +6,42 @@ import SimpleCard from '../../../../components/Common/SimpleCard/SimpleCard';
 
 // Icons & styles
 import styles from './SimpleProductDetail.module.scss';
+import { useRouter } from 'next/router';
 
 export default function SimpleProductDetail({
 	products,
 	openModal,
 	setOpenModal,
 }) {
+	const router = useRouter();
+	console.log('Router:', router);
+
 	return (
-		<>
-			<div>
-				<MetaHead title={'Hola'} />
-				<Carousel />
+		<div className='container'>
+			<MetaHead title={'Hola'} />
+			<Carousel />
 
-				<div className={styles.listContainer}>
-					<h4>Nombre de la categoría</h4>
+			<div className={styles.listContainer}>
+				<h4>Nombre de la categoría</h4>
 
-					{products.map((simpleProduct, index) => {
-						// Parseamos la info del objeto
-						const parseObj = [];
-						for (const property in simpleProduct) {
-							parseObj.push(simpleProduct[property]);
-						}
+				{products.map((simpleProduct, index) => {
+					// Parseamos la info del objeto
+					const parseObj = [];
+					for (const property in simpleProduct) {
+						parseObj.push(simpleProduct[property]);
+					}
 
-						return (
-							<SimpleCard
-								colums={parseObj}
-								onClick={() => setOpenModal(!openModal)}
-								key={index}
-							/>
-						);
-					})}
-				</div>
-
-				<Modal openModal={openModal} setOpenModal={setOpenModal} />
+					return (
+						<SimpleCard
+							colums={parseObj}
+							onClick={() => setOpenModal(!openModal)}
+							key={index}
+						/>
+					);
+				})}
 			</div>
-		</>
+
+			<Modal openModal={openModal} setOpenModal={setOpenModal} />
+		</div>
 	);
 }
