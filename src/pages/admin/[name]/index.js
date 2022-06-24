d89bdca8e695e9897bfa9312f9074e622fd564a0
BrunoAmerio <88524640+BrunoAmerio@users.noreply.github.com>
2022-06-23 22:26:02 -0300

    style: add more styles

--- src/pages/admin/[name]/index.js
+++ src/pages/admin/[name]/index.js
@@ -2,9 +2,12 @@
 import Cataloge from '../../../components/Common/Cataloge/Cataloge';
 import MetaHead from '../../../components/Common/MetaHead/MetaHead';
 import HeaderCustom from '../../../components/Common/HeaderCustom/HeaderCustom';
+import Link from 'next/link';
 
 // Icons
 import SettingsIcon from '@mui/icons-material/Settings';
+import AddIcon from '@mui/icons-material/Add';
+import style from '../../../styles/AdminHome.module.scss';
 
 // Data
 import catalogeData from '../../../data/cataloge.json';
@@ -20,6 +23,12 @@ const AdminHome = () => {
 			/>
 			<div className='container'>
 				<Cataloge data={catalogeData} />
+			</div>
+
+			<div className={style.addButton}>
+				<Link href='/admin/[name]/cardCreator' as='/admin/test/cardCreator'>
+					<AddIcon fontSize='large' />
+				</Link>
 			</div>
 		</div>
 	);
