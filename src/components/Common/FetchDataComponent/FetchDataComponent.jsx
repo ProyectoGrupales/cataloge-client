32ae56908165c09100e95499aa0a265ec4b6a879
BrunoAmerio <88524640+BrunoAmerio@users.noreply.github.com>
2022-07-08 12:28:46 -0300

    Admin restringed

--- src/components/Common/FetchDataComponent/FetchDataComponent.jsx
+++ src/components/Common/FetchDataComponent/FetchDataComponent.jsx
@@ -5,14 +5,21 @@ import { fetchSuccess } from '../../../redux/reducers/fetchUserData';
 import fetchCatalogeData from '../../../redux/apiCall/fetchCatalogeData';
 
 // Este componente no muestra nada pero se renderiza en todas las vistas
-// Se encarga de verificar si tenemos los datos del catálogo y en caso de no tenerlo realizamos la petición
+// Se encarga de la lógica de los datos que manejamos en aplicación
+// Como controlar las peticiones, o restringir el acceso a ciertas páginas
 const FetchDataComponent = () => {
 	const router = useRouter();
 	const dispatch = useDispatch();
 	const cataloge = useSelector(state => state.cataloge.catalogeData);
-	const user = useSelector(state => state.user.userData);
+	const user = JSON.parse(sessionStorage.getItem('userData'));
 
 	useEffect(() => {
+		if (router.pathname.includes('admin') && !user.name) {
+			window.location.href = '/';
+		} else {
+			dispatch(fetchSuccess(user));
+		}
+
 		if (!user || !user.name) {
 			// De está manera accedemos a la información del usuario, en caso de que se hayá logueado.
 			console.log('Estoy seteando el usuario');
